5b20797867e129e71f5813c4bd845afbdc1bbaea
SK <sarthakkarandikar03@gmail.com>
2022-09-20 06:48:36 +0530

    func

--- data_manager.py
+++ data_manager.py
@@ -2,15 +2,15 @@ import requests
 
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    
+
     def __init__(self):
-        sheet_end="https://api.sheety.co/8d8bd9e1daea2ae85b7b1cd31024a980/flightDeals/prices"
-        sheet_key="sjajdannenened"
-        sheet_headers={"Authorization":f"Bearer {sheet_key}"}
+        self.sheet_end="https://api.sheety.co/8d8bd9e1daea2ae85b7b1cd31024a980/flightDeals/prices"
+        self.sheet_key="sjajdannenened"
+        self.sheet_headers={"Authorization":f"Bearer {self.sheet_key}"}
 
     def retrive_data(self):
         response=requests.get(url=self.sheet_end,headers=self.sheet_headers)
-        return response["prices"]
+        return response.json()["prices"]
         
 
 
