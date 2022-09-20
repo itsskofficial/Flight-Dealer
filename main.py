a3866481e8447f088cc6b25a85c4c99b65220608
SK <sarthakkarandikar03@gmail.com>
2022-09-20 06:23:18 +0530

    added sheety

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+import requests
+
+SHEET_END="https://api.sheety.co/8d8bd9e1daea2ae85b7b1cd31024a980/flightDeals/prices"
+SHEET_KEY="sjajdannenened"
+sheet_headers={"Authorization":f"Bearer {SHEET_KEY}"}
+
+response=requests.get(url=SHEET_END,headers=sheet_headers)
+print(response.json())
